71fd8e68e092fa715697f4700197622b3fd4a8a5
anma6001 <43683052+anma6001@users.noreply.github.com>
2018-12-01 14:13:10 -0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,20 +1,15 @@
 create table Users(userID serial primary key, userName varchar(30) not null, password varchar(30) not null);
-insert into Users (userID,userName,password) values (%s,%s,%s);",
-                (DEFAULT,'user1','123'),(DEFAULT,'user2','password111'),
-                (DEFAULT,'user3','CUBoulder')
+insert into Users (userID,userName,password) values (%s,%s,%s);",(DEFAULT,'user1','123'),(DEFAULT,'user2','password111'),(DEFAULT,'user3','CUBoulder');
 
 create table Pictures(picID serial primary key, picName varchar(50) not null, userID int references Users (userID) on update cascade);
 insert into Pictures (picID,picName,userID) values (%s,%s,%s);",
                 (DEFAULT,'jpeg.jpeg','1'),(DEFAULT,'mom.jpeg','1'),(DEFAULT,'001.jpeg','1'),
                 (DEFAULT,'002.jpeg','2'),(DEFAULT,'003.jpeg','2'),(DEFAULT,'004.jpeg','2'),
-                (DEFAULT,'005.jpeg','3'),(DEFAULT,'006.jpeg','3'),(DEFAULT,'CU.jpeg','3')
+                (DEFAULT,'005.jpeg','3'),(DEFAULT,'006.jpeg','3'),(DEFAULT,'CU.jpeg','3');
 
 
 create table Tags(tagID serial primary key, tagName varchar(50) not null);
-insert into Tags (TagID, TagName) values (%s,%s);",(DEFAULT,'mom'),
-                (DEFAULT,'selfie'),(DEFAULT,'me'),(DEFAULT,'Chip')
+insert into Tags (TagID, TagName) values (%s,%s);",(DEFAULT,'mom'),(DEFAULT,'selfie'),(DEFAULT,'me'),(DEFAULT,'Chip');
 
-create table PicturesTags(picID int references Pictures (picID) on update cascade,
-	tagID int references Tags (tagID) on update cascade);
-insert into PicturesTags (picID,tagID) values (%s,%s);",(1,3),
-                (1,4),(2,2),(2,1),(3,3),(4,1),(5,3),(6,1),(6,2),(7,4),(8,3),(9,2)
+create table PicturesTags(picID int references Pictures (picID) on update cascade, tagID int references Tags (tagID) on update cascade);
+insert into PicturesTags (picID,tagID) values (%s,%s);",(1,3),(1,4),(2,2),(2,1),(3,3),(4,1),(5,3),(6,1),(6,2),(7,4),(8,3),(9,2);
