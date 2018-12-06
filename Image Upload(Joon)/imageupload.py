9c823ac300dc25b297dbabfd1daa650f98970a1a
Joon <sangjoon89@gmail.com>
2018-12-06 14:35:10 -0700

    Functional Upload and Display Image

--- Image Upload(Joon)/imageupload.py
+++ Image Upload(Joon)/imageupload.py
@@ -3,41 +3,31 @@ import os
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/home/user/Desktop/Joon/Uploads'
-ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif', 'txt'])
 
-app = Flask(__name__)
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__, static_folder = "Uploads")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-      return 'file uploaded successfully'
-"""
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return render_template('hello.html')
-"""
-        
+  target = os.path.join(UPLOAD_FOLDER, 'static/')
+
+  if not os.path.isdir(target):
+    os.mkdir(target)
+
+  for upload in request.files.getlist("file"):
+    filename = upload.filename
+    destination = "/".join([target,filename])
+    upload.save(destination)
+
+  return render_template("ImageDisplay.html", image_name = filename)
+
+
 @app.route('/')
 def welcome():
-    return render_template('ImageUpload.html')  # render a template
+    return render_template('ImageUploadog.html')  # render a template
 
 if __name__ == '__main__':
     app.run(debug=True)
